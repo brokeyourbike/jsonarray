b6235cf1f4324c8856a6ccca1f75639290a0234f
Ivan Stasiuk <brokeyourbike@gmail.com>
2023-05-09 15:48:33 +0100

    fix: update deps

--- go.mod
+++ go.mod
@@ -3,16 +3,18 @@ module github.com/brokeyourbike/jsonarray
 go 1.19
 
 require (
-	gorm.io/driver/mysql v1.4.3
-	gorm.io/gorm v1.24.1
+	github.com/stretchr/testify v1.8.2
+	gorm.io/driver/mysql v1.5.0
+	gorm.io/driver/sqlite v1.5.0
+	gorm.io/gorm v1.25.1
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- jsonarray_test.go
+++ jsonarray_test.go
@@ -1,17 +1,51 @@
 package jsonarray_test
 
 import (
+	"log"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/brokeyourbike/jsonarray"
 	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
-type Dummy struct {
-	Values []string
+var DB *gorm.DB
+
+func init() {
+	var err error
+	if DB, err = openTestConnection(); err != nil {
+		log.Printf("failed to connect database, got error %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func openTestConnection() (db *gorm.DB, err error) {
+	dialect := os.Getenv("GORM_DIALECT")
+
+	switch dialect {
+	default:
+		log.Println("testing sqlite3...")
+		db, err = gorm.Open(sqlite.Open(filepath.Join(os.TempDir(), "gorm.db")), &gorm.Config{})
+	}
+
+	if debug := os.Getenv("DEBUG"); debug == "true" {
+		db.Logger = db.Logger.LogMode(logger.Info)
+	} else if debug == "false" {
+		db.Logger = db.Logger.LogMode(logger.Silent)
+	}
+
+	return
 }
 
 func TestJSONArray(t *testing.T) {
+	type Dummy struct {
+		Values []string
+	}
+
 	a1 := jsonarray.JSONArray[int]{1, 2, 3}
 	a1 = append(a1, 4)
 	assert.Len(t, a1, 4)
@@ -22,4 +56,29 @@ func TestJSONArray(t *testing.T) {
 
 	d := Dummy{Values: a2}
 	assert.Len(t, d.Values, 4)
+}
+
+func TestJSONArrayI(t *testing.T) {
+	type UserWithJSON struct {
+		gorm.Model
+		Name string
+		Tags jsonarray.JSONArray[int]
+	}
+
+	DB.Migrator().DropTable(&UserWithJSON{})
+	assert.NoError(t, DB.Migrator().AutoMigrate(&UserWithJSON{}))
+
+	users := []UserWithJSON{{
+		Name: "json-1",
+		Tags: jsonarray.JSONArray[int]{1, 2},
+	}, {
+		Name: "json-2",
+		Tags: jsonarray.JSONArray[int]([]int{3, 4, 5}),
+	}}
+	assert.NoError(t, DB.Create(&users).Error)
+
+	var result UserWithJSON
+	assert.NoError(t, DB.First(&result, users[0].ID).Error)
+	assert.Equal(t, users[0].Name, result.Name)
+	assert.Equal(t, users[0].Tags[0], result.Tags[0])
 }
